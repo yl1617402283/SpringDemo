2391d428b4cad8c280084248067c3db06c67b106
chaizuyin@shuangchuangpuhui.com <chaizuyin@shuangchuangpuhui.com>
2018-01-24 10:54:20 +0800

    aware demo

---
+++ src/main/java/com/demo/springAware/AwareConfig.java
@@ -0,0 +1,11 @@
+package com.demo.springAware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.demo.springAware")
+public class AwareConfig {
+
+}
+
---
+++ src/main/java/com/demo/springAware/Main.java
@@ -0,0 +1,14 @@
+package com.demo.springAware;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main (String [] args){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AwareConfig.class);
+
+        AwareService awareService = context.getBean(AwareService.class);
+        awareService.outputResult();
+
+        context.close();
+    }
+}
